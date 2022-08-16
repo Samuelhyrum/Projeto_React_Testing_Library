6dd99ba373e9a9979004894f1dd8c525ec2732f9
Samuel Hyrum <samuelhymp@gmail.com>
2022-08-16 17:22:35 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Requisito 3', () => {
+  test('Teste o componente <Favorites.js />.', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const favorites = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    expect(favorites).toBeInTheDocument();
+    userEvent.click(favorites);
+
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+
+    const text = screen.getByText('No favorite pokemon found');
+    expect(text).toBeInTheDocument();
+  });
+});
