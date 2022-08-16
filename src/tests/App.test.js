e7b967b8e37ea286f3d86ee6cf728c35174e35c7
Samuel Hyrum <samuelhymp@gmail.com>
2022-08-16 16:35:34 -0300

    Requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,29 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Requisito 1', () => {
+  test('Teste se o topo da aplicação contém um conjunto fixo de links', () => {
+    renderWithRouter(<App />);
+    const home = screen.getByRole('link', { name: 'Home' });
+    const about = screen.getByRole('link', { name: 'About' });
+    const favorite = screen.getByRole('link', { name: 'Favorite Pokémons' });
+
+    expect(home).toBeInTheDocument();
+    expect(about).toBeInTheDocument();
+    expect(favorite).toBeInTheDocument();
+  });
+
+  //   test('teste o redirecionamento para a pagina inicial', () => {
+  //     const { history } = renderWithRouter(<App />);
+
+  //     const home = screen.getByRole('link', { name: 'Home' });
+  //     expect(home).toBeInTheDocument();
+  //     userEvent.click(home);
+
+//     const { pathname } = history.location;
+//     expect(pathname).toBe('/');
+//   });
+});
